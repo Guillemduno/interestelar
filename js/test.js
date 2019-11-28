b297d431620a74b3d0e817315d43770a70cbba92
josan <josanescod@gmail.com>
2019-11-28 08:41:48 +0100

    boton menu

--- js/test.js
+++ js/test.js
@@ -1,6 +1,33 @@
+var botonActivo = false;
 
+window.onload = () => {
+  botonMenu();
+  console.log("estado del boton:", botonActivo);
+};
 
-window.onload=()=>{
+window.onresize = () => {
+  if (window.innerWidth >= 820) {
+    let menu = document.querySelector("nav");
+    console.log("resolucion >=820px ocultando menu...");
+    menu.style.left = "-85%";
+    botonActivo = false;
+  }
+};
 
-    console.log('hello world')
+function botonMenu() {
+  let boton = document.querySelector("#hamburguesa");
+  boton.addEventListener("click", menuAccion);
+}
+
+function menuAccion() {
+  let menu = document.querySelector("nav");
+  if (botonActivo == false) {
+    botonActivo = true;
+    console.log("se ha pulsado el boton del Menu", botonActivo);
+    menu.style.left = "0%";
+  } else {
+    botonActivo = false;
+    console.log("se ha pulsado el boton del Menu", botonActivo);
+    menu.style.left = "-85%";
+  }
 }
